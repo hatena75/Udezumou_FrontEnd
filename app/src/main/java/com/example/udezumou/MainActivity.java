a065d485d966e66cc9d032afb2342f73fe602524
hatena75 <hatena75@outlook.jp>
2021-06-19 16:41:45 +0900

    バックエンドとの連携完了

--- app/src/main/java/com/example/udezumou/MainActivity.java
+++ app/src/main/java/com/example/udezumou/MainActivity.java
@@ -39,8 +39,9 @@ public class MainActivity extends AppCompatActivity{
     private ProgressBar bar;
 
     private boolean isFight = false;
+    private boolean bePlayer2 = false;
 
-    private static final String URI_SERVER = "http://192.168.32.32:8080";
+    private static final String URI_SERVER = "http://192.168.11.30:8080";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -153,24 +154,33 @@ public class MainActivity extends AppCompatActivity{
                     public void run()
                     {
                         Toast.makeText( MainActivity.this, "Match!", Toast.LENGTH_SHORT ).show();
+                        Log.d("Matched", "match success");
                         isFight = true;
                         StateTextView.setText("Fight!");
-                        JSONObject objMessage = (JSONObject)args[0];
+                        Log.d("Matched", (String) args[0]);
+                        JSONObject objMessage = null;
                         try
                         {
+                            objMessage = new JSONObject((String) args[0]);
+                            Log.d("JSON", objMessage.getString("name2"));
                             roomID = objMessage.getString("roomId");
                             if(objMessage.getString("name1").equals(m_strNickname)){
                                 OpponentNameTextView.setText(objMessage.getString("name2"));
+                                bePlayer2 = false;
                             }
                             else
                             {
                                 OpponentNameTextView.setText(objMessage.getString("name1"));
+                                bePlayer2 = true;
                             }
                         }
                         catch( JSONException e )
                         {
                             e.printStackTrace();
                         }
+
+                        //battleメッセージの送信を開始
+                        getMuscleLoop.post(r);
 
                     }
                 } );
@@ -188,14 +198,24 @@ public class MainActivity extends AppCompatActivity{
                     @Override
                     public void run()
                     {
-                        JSONObject objMessage = (JSONObject)args[0];
+                        JSONObject objMessage = null;
                         try
                         {
                             //各々のパワー表示を更新
-                            String userPow = objMessage.getString("userName");
-                            MyPowerTextView.setText(userPow);
-                            OpponentPowerTextView.setText(objMessage.getString("opponent"));
-                            bar.setProgress(Integer.parseInt(userPow));
+                            objMessage = new JSONObject((String) args[0]);
+                            String userGage;
+                            String opponentGage;
+                            if(!bePlayer2){
+                                userGage = objMessage.getString("userName");
+                                opponentGage = objMessage.getString("opponent");
+                            }else{
+                                userGage = objMessage.getString("opponent");
+                                opponentGage = objMessage.getString("userName");
+                            }
+
+                            MyPowerTextView.setText(userGage);
+                            OpponentPowerTextView.setText(opponentGage);
+                            bar.setProgress(Integer.parseInt(userGage));
                         }
                         catch( JSONException e )
                         {
@@ -219,14 +239,22 @@ public class MainActivity extends AppCompatActivity{
                     public void run()
                     {
                         Toast.makeText( MainActivity.this, "Finish!", Toast.LENGTH_SHORT ).show();
-                        JSONObject objMessage = (JSONObject)args[0];
+                        JSONObject objMessage = null;
                         try
                         {
                             //各々のパワー表示を更新
-                            String userGage = objMessage.getString("userName");
-                            String opponentGage = objMessage.getString("opponent");
+                            objMessage = new JSONObject((String) args[0]);
+                            String userGage;
+                            String opponentGage;
+                            if(!bePlayer2){
+                                userGage = objMessage.getString("userName");
+                                opponentGage = objMessage.getString("opponent");
+                            }else{
+                                userGage = objMessage.getString("opponent");
+                                opponentGage = objMessage.getString("userName");
+                            }
                             MyPowerTextView.setText(userGage);
-                            OpponentPowerTextView.setText(objMessage.getString(opponentGage));
+                            OpponentPowerTextView.setText(opponentGage);
                             bar.setProgress(Integer.parseInt(userGage));
                             if(Integer.parseInt(userGage) > Integer.parseInt(opponentGage)){
                                 StateTextView.setText("You Win!");
@@ -235,6 +263,7 @@ public class MainActivity extends AppCompatActivity{
                                 StateTextView.setText("You Lose!");
                             }
                             isFight = false;
+                            //getMuscleLoop.removeCallbacks(r);
                             //通信切断
                             m_socket.disconnect();
                         }
@@ -265,6 +294,7 @@ public class MainActivity extends AppCompatActivity{
         public void run() {
             // UIスレッド
             if (!isFight) { //戦い終わったら抜ける
+                Log.d("Cancel", "runnuble return");
                 return;
             }
             //HTTPのGETメソッドで情報を取得
@@ -273,7 +303,8 @@ public class MainActivity extends AppCompatActivity{
             //Log.d("GET", MyPowerTextView.getText().toString());
             Random rand = new Random();
             int myPower = rand.nextInt(1023); //仮の乱数
-            //int myPower = Integer.parseInt(MyPowerTextView.getText().toString());
+            //int myPower = Integer.parseInt(MyMuscleTextView.getText().toString());
+            MyMuscleTextView.setText(String.valueOf(myPower));
 
             // サーバーに情報を送信する。
             //msg.value,mgs.roomId,msg.userName,
